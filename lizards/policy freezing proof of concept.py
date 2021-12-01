fa1a25f8999cb85f65d0ac1de03c400b2fc95652
ymh <hanyongming@gmail.com>
2021-12-01 14:56:26 -0500

    added detailed to dos; also starting to integrate stuff from testing stuff

--- lizards/policy freezing proof of concept.py
+++ lizards/policy freezing proof of concept.py
@@ -21,6 +21,15 @@ from ray.tune import CLIReporter, register_env
 # https://github.com/ray-project/ray/blob/fd13bac9b3fc2e7142065c759f2c9fc1c753e912/rllib/examples/self_play_league_based_with_open_spiel.py
 # https://github.com/ray-project/ray/blob/fd13bac9b3fc2e7142065c759f2c9fc1c753e912/rllib/examples/self_play_with_open_spiel.py
 
+""" TO DO WED Dec 1
+1. Test training loop with APTCallback_BA_simplest_for_quicktest to check if policy freezing stuff has been recorded properly
+2. Run each exp for couple of iters and use new policy freezing logging to check if freezing happening as desired.
+3. Add checkpoint saving and evaluation (see experiments.py for how)
+4. chk with Qiuhong / Anna re training architecture
+5. Run the experiments for full iter length.
+"""
+
+
 
 # TO DO NOTE: We should try to switch over to the new training conv stuff that Qiuhong / Anna has made before actually training the experiments
 convs = {"adversarial-pursuit": [[13, 10, 1]],
@@ -29,6 +38,10 @@ convs = {"adversarial-pursuit": [[13, 10, 1]],
          "tiger-deer": [[9, 9, 1]],
          "combined-arms": [[25, 13, 1]]}
 
+
+def list_map(*args): return list(map(*args))
+def tup_map(*args): return tuple(map(*args))
+def np_itermap(*args, dtype=bool): return np.fromiter(map(*args), dtype=dtype)
 
 def train_for_testing_pol_wt_freezing(trainer, num_iters=20, log_intervals=10, log_dir=Path("logs/pol_freezing")):
 
@@ -75,50 +88,15 @@ Assumptions:
 * names of policies == prefixes of teams (i.e., either "predator" or "prey")
 """
 
-class TwoTeamOnTrainResultMixin:   
-    def get_other_team(self):
-        # this will need to be made more robust later
-        other_team = self.teams[0] if self.teams[0] not in self.curr_trainable_policies else self.teams[1]
-        return other_team
 
-    def on_train_result(self, *, trainer, result, **kwargs):
-        """ will be called at the end of Trainable.train(), so that the first time this is called, trainer.iteration will == 1. 
-        (Iteration 0 is the state when *no* training has been done.)"""
-        
-        curr_iter = trainer.iteration
-        if curr_iter > self.burn_in_iters and ((curr_iter - self.burn_in_iters) % self.interval_len == 0):
-            team_to_freeze = self.curr_trainable_policies
-            team_to_train = self.get_other_team()
-            print(team_to_train)
-            self.curr_trainable_policies = {team_to_train}
-
-            print(f"On iter {curr_iter + 1}, {team_to_freeze} will be frozen, {team_to_train} will be trained")
-
-            def _set(worker):
-                # Note: `_set` must be enclosed in `on_train_result`!
-                worker.set_policies_to_train(self.curr_trainable_policies)
-            
-            trainer.workers.foreach_worker(_set)
-
-
-# APTCallback stands for: Alternating Policy Training Callback
-class APTCallback_BF(DefaultCallbacks):
-    # We'll make this cleaner by using the mixin once we're sure that this works
-
+class APTCallback_BA_simplest_for_quicktest(DefaultCallbacks): 
     def __init__(self):
         super().__init__()
 
         self.teams = ("red", "blue") 
         self.curr_trainable_policies = {"red"} 
-        # Start with red being the one whose policy's being trained
 
-        self.burn_in_iters = 4 
-        # How many iterations to train normally for, before starting alternating policy training/freezing regimen
-
-        self.interval_len = 4
-        # The point of this, recall, is to train one team while keeping the other frozen for regular intervals
-        # interval_len := number of iterations that each interval consists in
-
+        self.interval_len = 2
 
     def get_other_team(self): 
         # Returns str
@@ -127,17 +105,17 @@ class APTCallback_BF(DefaultCallbacks):
         return other_team
 
     def on_train_result(self, *, trainer, result, **kwargs):
-                """ will be called at the end of Trainable.train(), so that the first time this is called, trainer.iteration will == 1. 
-        (Iteration 0 is the state when *no* training has been done.)"""
+        """ will be called at the end of Trainable.train(), so that the first time this is called, trainer.iteration will == 1"""
         
         curr_iter = trainer.iteration
         print(f"Just finished train iter {curr_iter}")
-        if curr_iter > self.burn_in_iters and ((curr_iter - self.burn_in_iters) % self.interval_len == 0):
+
+        if curr_iter == 1 or curr_iter % self.interval_len == 0:
             team_to_freeze = self.curr_trainable_policies
             team_to_train = self.get_other_team()  # this will be str
             self.curr_trainable_policies = {team_to_train}
 
-            print(f"On iter {curr_iter + 1}, {team_to_freeze} will be frozen, {team_to_train} will be trained")
+            print(f"For iter {curr_iter + 1}: Freezing {team_to_freeze} and training {team_to_train}")
 
             def _set(worker):
                 print(f"_set has been called; self.curr_trainable_policies are {self.curr_trainable_policies}")
@@ -146,8 +124,9 @@ class APTCallback_BF(DefaultCallbacks):
             
             trainer.workers.foreach_worker(_set)
 
+
 # APTCallback stands for: Alternating Policy Training Callback
-class APTCallback_BF_to_wrap(DefaultCallbacks):
+class APTCallback_BA_to_wrap(DefaultCallbacks):
     # We'll make this cleaner by using the mixin once we're sure that this works
 
     def __init__(self, team_to_turn_length: dict):
@@ -183,52 +162,37 @@ class APTCallback_BF_to_wrap(DefaultCallbacks):
 
             trainer.workers.foreach_worker(_set)
 
-class APTCallback_AP(DefaultCallbacks):
-
-    def __init__(self):
-        super().__init__()
-
-        self.teams = ("predator", "prey") 
-        self.curr_trainable_policies = {"predator"} 
-        # Start with predator being the one whose policy's being trained
-
-        self.burn_in_iters = 0 
-        # How many iterations to train normally for, before starting alternating policy training/freezing regimen
-
-        self.interval_len = 3
-        # The point of this, recall, is to train one team while keeping the other frozen for regular intervals
-        # interval_len := number of iterations that each interval consists in
-
-    
-    def get_other_team(self): 
-        # Returns str
-        # this will need to be made more robust later
-        other_team = self.teams[0] if self.teams[0] not in self.curr_trainable_policies else self.teams[1]
-        return other_team
-
-    def on_train_result(self, *, trainer, result, **kwargs):
-        """ will be called at the end of Trainable.train(), so that the first time this is called, trainer.iteration will == 1. 
-        (Iteration 0 is the state when *no* training has been done.)"""
-        
-        curr_iter = trainer.iteration
-        if curr_iter > self.burn_in_iters and ((curr_iter - self.burn_in_iters) % self.interval_len == 0):
-            team_to_freeze = self.curr_trainable_policies
-            team_to_train = self.get_other_team()  # this will be str
-            self.curr_trainable_policies = {team_to_train}
-
-            print(f"On iter {curr_iter + 1}, {team_to_freeze} will be frozen, {team_to_train} will be trained")
-
-            def _set(worker):
-                print(f"_set has been called; self.curr_trainable_policies are {self.curr_trainable_policies}")
-                # Note: `_set` must be enclosed in `on_train_result`!
-                worker.set_policies_to_train(self.curr_trainable_policies)
-            
-            trainer.workers.foreach_worker(_set)
 
 # Demonstration of how to make code more reusable with mixins:
 # I'm not 100% sure the mixin will work well with Ray though, so caveat emptor 
 # Tho the mixin does at least seem to add the relevant methods
 # This is definitely the way to make the code more reusable though --- just needs more testing first
+
+# class TwoTeamOnTrainResultMixin:   
+#     def get_other_team(self):
+#         # this will need to be made more robust later
+#         other_team = self.teams[0] if self.teams[0] not in self.curr_trainable_policies else self.teams[1]
+#         return other_team
+
+#     def on_train_result(self, *, trainer, result, **kwargs):
+#         """ will be called at the end of Trainable.train(), so that the first time this is called, trainer.iteration will == 1. 
+#         (Iteration 0 is the state when *no* training has been done.)"""
+        
+#         curr_iter = trainer.iteration
+#         if curr_iter > self.burn_in_iters and ((curr_iter - self.burn_in_iters) % self.interval_len == 0):
+#             team_to_freeze = self.curr_trainable_policies
+#             team_to_train = self.get_other_team()
+#             print(team_to_train)
+#             self.curr_trainable_policies = {team_to_train}
+
+#             print(f"On iter {curr_iter + 1}, {team_to_freeze} will be frozen, {team_to_train} will be trained")
+
+#             def _set(worker):
+#                 # Note: `_set` must be enclosed in `on_train_result`!
+#                 worker.set_policies_to_train(self.curr_trainable_policies)
+            
+#             trainer.workers.foreach_worker(_set)
+
 # class APTCallback_AP(DefaultCallbacks, TwoTeamOnTrainResultMixin):
 
 #     def __init__(self):
@@ -249,36 +213,40 @@ class APTCallback_AP(DefaultCallbacks):
 
 # env-specific things
 # ===================
-def AP_alternating_pol_train_PROTOTYPE(map_size=25, *args):
-    """ This uses policy sharing for each of the teams to keep things simple """
 
-    # training_setup = {"num_iters": 4,
-    #                  # "log_intervals": 1,
-    #                  "log_dir": os.path.join(os.path.dirname(os.path.dirname(__file__)), 'logs/AP_alternating_policy_freezing_proof_concept')}
+def BA_pol_mapping_fn(agent_id, episode, worker, **kwargs):
+    return "red" if agent_id.startswith("red") else "blue"
 
-    conv_filters = None
+def BA_apt_1_30_PROTOTYPE(map_size=19, *args):
+    # started Wed Dec 1
+    env_name = "battle"
 
-    env_name = "adversarial-pursuit"
+    training_setup = {"num_iters": 16,
+                     "log_intervals": 4,
+                     "log_dir": 'logs/BA_testing'}
+
     env_fn = env_directory[env_name]
-    env_config = {'map_size': map_size} # min map sz is 7 for AP
-
+    env_config = {'map_size': map_size} 
     action_space, obs_space = env_spaces[env_name]["action_space"], env_spaces[env_name]["obs_space"]
 
-    def pol_mapping_fn(agent_id, episode, worker, **kwargs):
-        return "predator" if agent_id.startswith("predator") else "prey"
-    
+    class APTCallback_BA_test_1_30(APTCallback_BA_to_wrap):
+        def __init__(self):
+            super().__init__({"red": 1, "blue": 30})
+
     ray_trainer_config = {
 
-        "callbacks": APTCallback_AP, # IMPT
+        "callbacks": APTCallback_BA_test_1_30, # IMPT 
+        # "callbacks": APTCallback_BF_test_1,
 
-        "env": env_name,
         "multiagent": {
-            "policies": {"predator": (None, obs_space, action_space, dict()),
-                         "prey": (None, obs_space, action_space, dict())},
-            "policy_mapping_fn": pol_mapping_fn
+            "policies": {"red": (None, obs_space, action_space, dict()),
+                         "blue": (None, obs_space, action_space, dict())},
+            "policy_mapping_fn": BA_pol_mapping_fn
         },
+        
+        "env": env_name,
         "model": {
-            "conv_filters": convs[env_name] if conv_filters is None else conv_filters
+            "conv_filters": convs[env_name]
         },
         "env_config": env_config,
         "create_env_on_driver": True, # potentially disable this?
@@ -287,16 +255,8 @@ def AP_alternating_pol_train_PROTOTYPE(map_size=25, *args):
     }
 
     trainer = ppo.PPOTrainer(config=ray_trainer_config)
+    train_for_testing_pol_wt_freezing
 
-    # log_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'logs/Some_checkpoint_filename')
-    #checkpoint = train_ray_trainer(trainer, num_iters=training_setup["train_iters"], 
-                                           # log_intervals=training_setup['log_intervals'], 
-                                            #log_dir=training_setup["log_dir"])
-    checkpoint = train_ray_trainer(trainer)
-
-
-    # if checkpoint:
-        # render_from_checkpoint(checkpoint, trainer, env_fn, env_config, policy_fn)
 
 
 
@@ -324,6 +284,7 @@ def BF_alternating_pol_training_PROTOTYPE(map_size=50, *args):
 
         # "callbacks": APTCallback_BF, # IMPT # Testing new callback below (Eli):
         "callbacks": APTCallback_BF_test_1,
+        "policies_to_train": ["red"], # start by training red for 1 iter
 
         "multiagent": {
             "policies": {"red": (None, obs_space, action_space, dict()),
