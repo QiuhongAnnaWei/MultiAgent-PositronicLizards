2ac81f58b53dc8464ceeb027e874e33c6d427459
ymh <hanyongming@gmail.com>
2021-12-01 20:47:35 -0500

    cleaned some helper func code up a little, added tests back and added more tests; also improved `get_changepoints`

--- lizards/policy_freezing_helper.py
+++ lizards/policy_freezing_helper.py
@@ -1,19 +1,30 @@
-from ray.tune.trainable import Trainable
+# Please either leave tests in file or put them in another file and have them run automatically
+# These tests serve not just as tests, but also as documentation / explanations of how the functions work
+# If you are wondering why a testing framework was not used: we're rushing for time here.
 
+from ray.tune.trainable import Trainable
 
 import pytz
 from datetime import datetime
 from copy import deepcopy
 import pandas as pd
 from typing import List, Dict
+from collections.abc import Iterable
+
 import numpy.typing as npt
 import numpy as np
 from functools import partial
 from pathlib import Path
 
-def list_map(*args): return list(map(*args))
-def tup_map(*args): return tuple(map(*args))
-def np_itermap(*args, dtype=bool): return np.fromiter(map(*args), dtype=dtype)
+def map_wrapper(*args, collector_func): return collector_func(map(*args))
+list_map = partial(map_wrapper, collector_func=list)
+tup_map = partial(map_wrapper, collector_func=tuple)
+
+def get_timestamp():
+    tz = pytz.timezone('US/Eastern')
+    short_timestamp = datetime.now(tz).strftime("%d.%m_%H.%M")
+    return short_timestamp
+
 
 def get_weights(trainer: Trainable, team: str):
     """
@@ -23,31 +34,98 @@ def get_weights(trainer: Trainable, team: str):
     """
     return trainer.get_policy(team).get_weights()
 
-def get_timestamp():
-    tz = pytz.timezone('US/Eastern')
-    short_timestamp = datetime.now(tz).strftime("%d.%m_%H.%M")
-    return short_timestamp
 
 def pairwise_eq_chk(team_name, wt_dict1, wt_dict2):
     """
     Returns True iff wts for team_name in both dicts are the same  
     Assumes keys are the same for both dicts; see examples below 
     """
-    d1 = wt_dict1[team_name]
-    d2 = wt_dict2[team_name]
+    wt_dict1 = wt_dict1[team_name]
+    wt_dict2 = wt_dict2[team_name]
 
-    return np.array([True if np.array_equal(d1[key], d2[key]) else False for key in d1]).all()
+    bool_list_from_wtname_map = [True if np.array_equal(wt_dict1[wt_name], wt_dict2[wt_name]) else False for wt_name in wt_dict1]
+
+    return np.array(bool_list_from_wtname_map).all()
+
+def get_test_base_egs():
+    test_dict_blue_123 = {"blue": {'blue/conv_value_1/bias': np.array([1, 2, 3])}}
+    test_dict_blue_123_copy = deepcopy(test_dict_blue_123)
+    test_dict_blue_321 = {"blue": {'blue/conv_value_1/bias': np.array([3, 2, 1])}}
+    test_dict_blue_321_copy = deepcopy(test_dict_blue_321)
+
+    return test_dict_blue_123, test_dict_blue_123_copy, test_dict_blue_321, test_dict_blue_321_copy
+
+def get_test_br_egs():
+    test_dict_br_123 = {"blue": {'blue/conv_value_1/bias': np.array([1, 2, 3])},
+                        "red": {'red/conv_value_1/bias': np.array([4, 9])}}
+    test_dict_br_123_copy = deepcopy(test_dict_br_123)
+    test_dict_br_321 = {"blue": {'blue/conv_value_1/bias': np.array([3, 2, 1])},
+                        "red": {'red/conv_value_1/bias': np.array([4, 9])}}
+    test_dict_br_321_copy = deepcopy(test_dict_br_321)
+
+    return test_dict_br_123, test_dict_br_123_copy, test_dict_br_321, test_dict_br_321_copy
+
+
+def test_pairwise_eq_chk():
+    test_dict_blue_123, test_dict_blue_123_copy, test_dict_blue_321, test_dict_blue_321_copy = get_test_base_egs()
+
+    assert pairwise_eq_chk("blue", test_dict_blue_123, test_dict_blue_123_copy) == True
+    assert pairwise_eq_chk("blue", test_dict_blue_123, test_dict_blue_321) == False
+
+test_pairwise_eq_chk()
+
 
 def check_eq_policy_wts_across_iters(pol_wts_across_iters: List[Dict[str, Dict[str, npt.ArrayLike]]], team_names: List[str]):
     """ 
     Given a list of policy weights across iterations, checks if the pol wts at each iteration is equal to the previous one
     pol_wts_across_iters's first value must be the initial random wts for each team; i.e., the wts at iteration 0 
-    So at idx i of pol_wts_across_iters, we'll have the pol weights __at the end of__ the i-th iteration (where the idxing is 0-based)
+    So at idx i of pol_wts_across_iters, we'll have the pol weights from __the end of__ the i-th iteration (where the idxing is 0-based)
     """
-    return {team: tup_map(partial(pairwise_eq_chk, team), pol_wts_across_iters, pol_wts_across_iters[1:]) for team in team_names}
+    tup_map(partial(pairwise_eq_chk, team), pol_wts_across_iters, pol_wts_across_iters[1:])
+    return {team:  for team in team_names}
 
-def get_changepoints(eq_chk_dict: dict):
-    # False here means: the pol wts at that iteration not equal to those at previous iter
-    return {team: np.nonzero(np.array(eq_chk_dict[team])==False) for team in eq_chk_dict}
+def test_check_eq_policy_wts_across_iters():
+    test_dict_br_123, test_dict_br_123_copy, test_dict_br_321, test_dict_br_321_copy = get_test_br_egs()
+    test_pw_across_iters = [test_dict_br_123, test_dict_br_123_copy, test_dict_br_321, test_dict_br_321_copy]
+    test_pw_eq_chk_dict = {'blue': (True, False, True), 'red': (True, True, True)}
+    assert check_eq_policy_wts_across_iters(test_pw_across_iters, ["blue", "red"]) == test_pw_eq_chk_dict
+
+test_check_eq_policy_wts_across_iters()
+
+
+def get_changepoints(eq_chk_dict: Dict[str, Iterable[bool]]):
+    """
+    Expects a dict with the structure: team_name [str] => Tuple of bools
+    False here means: the pol wts at that iteration not equal to those at previous iter
+    """
+    changepts_vec_for_team = lambda team: np.flatnonzero(np.array(eq_chk_dict[team])==False) 
+
+    return {team: changepts_vec_for_team(team) for team in eq_chk_dict}
+
+def valarray_eq(dict1: Dict[str, npt.ArrayLike], dict2: Dict[str, npt.ArrayLike]):
+    """ Given two dicts with same keys, check their associated np arrays for equality """
+    bool_list_from_key_map = [True if np.array_equal(dict1[key], dict2[key]) else False for key in dict1]
+
+    return np.array(bool_list_from_key_map).all()
+
+
+
+test_pw_eq_chk_dict0 = {'blue': (), 'red': ()}
+test_pw_eq_chk_dict1 = {'blue': (True, False, True), 'red': (True, True, True)}
+test_pw_eq_chk_dict2 = {'blue': (True, False, True, False, False), 'red': (True, True, True, True, False)}
+
+assert valarray_eq(test_pw_eq_chk_dict0, {'blue': np.array([]), 'red': np.array([])}) == True 
+assert valarray_eq(get_changepoints(test_pw_eq_chk_dict1), {'blue': np.array([1]), 'red': np.array([])}) == True
+
+
+
+
+
+
+
+
+
+
+
 
 
