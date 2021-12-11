80ef5e438a9abfbaf7118efb891bc19189ab6e09
ymh <hanyongming@gmail.com>
2021-12-11 01:26:28 -0500

    Started, but haven't finished, adapting viz saving functionality for stats collection eval fn

--- lizards/quantitative_analysis/stats.py
+++ lizards/quantitative_analysis/stats.py
@@ -7,8 +7,28 @@ from collections import defaultdict
 import pandas as pd
 from pathlib import Path
 
+def save_eval_viz(save_dir, save_file_prefix, diff_frame_list):
+    save_dir = Path(save_dir)
+    mp4_save_path = save_dir.joinpath("{save_file_prefix}.mp4")
 
-def collect_stats_from_eval(checkpoint_path, trainer, env, env_config, policy_fn, max_iter=2 ** 8, is_battle=True, log_dir = None, save_df = True):
+    # TO DO
+    pass
+
+
+    
+    # save_path = f"{logname}.gif"
+    # log(f"{logname}.txt", f"\n# Saving gif to: {save_path}")
+    # diff_frame_list[0].save(save_path, save_all=True, append_images=diff_frame_list[1:], duration=100, loop=0)
+    # save_path = f"{logname}.mp4"
+    # log(f"{logname}.txt", f"\n# Saving video to: {save_path}\n")
+    # video = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), 1, (width, height))
+    # for i, image in enumerate(diff_frame_list):
+    #     video.write(cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
+    # for i in [0, len(diff_frame_list)-1]:
+    #     diff_frame_list[i].save(f"{logname}_{i}.jpg")
+
+
+def collect_stats_from_eval(checkpoint_path, trainer, env, env_config, policy_fn, max_iter=2 ** 8, is_battle=True, log_dir=None, eval_id="", save_df=True, save_viz=False):
     if checkpoint_path:
         trainer.restore(checkpoint_path)
 
@@ -19,17 +39,18 @@ def collect_stats_from_eval(checkpoint_path, trainer, env, env_config, policy_fn
     env = env.env(**env_config)
     env = ss.pad_observations_v0(env)
     env = ss.pad_action_space_v0(env)
-
-    i = 0
-    attacksPerAgent = defaultdict(int)
-    attacksTotal = defaultdict(list)
-    attacksPerTeam = defaultdict(int)
-
     env.reset()
 
+    if save_viz:
+        diff_frame_list = []
+        width, height, img = None, None, None
+
+    attacksPerAgent, attacksTotal, attacksPerTeam = defaultdict(int), defaultdict(list), defaultdict(int)
+
+    i = 0
     for agent in env.agent_iter(max_iter=max_iter):
         if i % 1000 == 0:
-            print(f"getting actions for: iteration {i} ...")
+            print(f"getting actions for: iter {i} ...")
         observation, reward, done, info = env.last() # (observation[:,:,3/4]==0).sum()
         if done:
             action = None
@@ -56,9 +77,25 @@ def collect_stats_from_eval(checkpoint_path, trainer, env, env_config, policy_fn
             break
         # out = False
         env.step(action)
+        if save_viz:
+            img2 = PIL.Image.fromarray(env.render(mode='rgb_array'))
+            if img is None:
+                width, height = img2.width, img2.height
+                img = np.zeros((width, height))
+            if np.array_equal(np.array(img), np.array(img2)) == False:
+                img = img2.copy()
+                ImageDraw.Draw(img2).text( (2, height-10), f"iter={i}", (0,0,0))
+                if is_battle:
+                    ImageDraw.Draw(img2).text( (2, 13), f"HP={str(round((s[:,:,2]).sum(), 2))}",  (0,0,0)) 
+                    ImageDraw.Draw(img2).text( (width-55, 13), f"HP={str(round((s[:,:,4]).sum(), 2))}",  (0,0,0)) # "{:.4f}".format()
+                diff_frame_list.append(img2)
         i += 1
+    
     env.close()
+    if save_viz: save_eval_viz(save_dir, f"viz_{eval_id}", diff_frame_list)
 
+
+    # Will prob move this df-making code out once I add the HP code
     # make the df; using the concat method coz arrays not of same len
     list_of_atks_across_timesteps_per_agent_series = [pd.Series(atks_across_timesteps, name=agent_nm) for agent_nm, atks_across_timesteps in attacksTotal.items()]
     attacksDf = pd.concat(list_of_atks_across_timesteps_per_agent_series, axis=1)
